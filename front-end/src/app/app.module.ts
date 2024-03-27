247d69f8624066bc9a1ed9a1dbaa9276ad0b9484
Vince Gruse <grusvin10@gmail.com>
2024-03-26 20:31:20 -0400

    Initial commit
    Signed-off-by: Vince Gruse <grusvin10@gmail.com>

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HomeModule} from "./home/home.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {HomeModule} from "./home/home.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
