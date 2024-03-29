191da99fb19a605f2f5a5f7c7585c8c27e09cbc5
Vince Gruse <grusvin10@gmail.com>
2024-03-29 14:40:47 -0400

    Fixed issue with constructor that was allowing ingredients to persist on a reload
    Signed-off-by: Vince Gruse <grusvin10@gmail.com>

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -16,7 +16,6 @@ export class HomeComponent implements OnInit{
     private ingredientService: IngredientService,
     private router: Router
   ) {
-    this.ingredients = this.ingredientService.getIngredients();
   }
 
   ngOnInit() {
