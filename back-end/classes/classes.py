0470cf625eb33beb29dbf2d533ffdda558770b08
rahal <rahalncm@gmail.com>
2024-03-30 21:42:00 -0400

    added base classes

---
+++ back-end/classes/classes.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class BaseRecipe(BaseModel):
+    name: str
+    id: int
+    description: str
+    image: str
+
+
+class BaseIngredient(BaseModel):
+    id: int
+    name: str
+    description: str
+    image: str
+
