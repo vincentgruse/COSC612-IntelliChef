b65ccaf45071f717ed6f44a7d755ebe19e9baa56
Vincent Gruse <grusvin10@gmail.com>
2024-04-13 17:26:55 -0400

    Add support for individual recipe page

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import { RecipesComponent } from "./recipes/recipes.component";
+import {RecipeIndividualComponent} from "./recipe-individual/recipe-individual.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: "home", component: HomeComponent},
-  {path: "recipes", component: RecipesComponent}
+  {path: "recipes", component: RecipesComponent},
+  {path: "recipe-individual", component: RecipeIndividualComponent}
 ];
 
 @NgModule({
