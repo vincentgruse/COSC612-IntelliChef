efdd5e052baf44c58a039e7f29735dac9c32c776
Vincent Gruse <grusvin10@gmail.com>
2024-04-14 05:42:18 -0400

    removed unused import

--- front-end/src/app/home/home.module.ts
+++ front-end/src/app/home/home.module.ts
@@ -3,9 +3,6 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { HomeComponent } from './home.component';
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
-import { RecipeDetailComponent } from '../recipe-detail/recipe-detail.component';
-
-
 
 @NgModule({
   declarations: [
