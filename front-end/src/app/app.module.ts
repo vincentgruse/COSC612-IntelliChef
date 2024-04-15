9b4d86fb97cdfe8b314573c3293be7ba4b1ad49b
Vince Gruse <grusvin10@gmail.com>
2024-04-15 18:22:56 -0400

    formatting changes

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -8,18 +8,18 @@ import { RecipesComponent } from './recipes/recipes.component';
 import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RecipesComponent,
-    RecipeDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        RecipesComponent,
+        RecipeDetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HomeModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
