4822253922f73b63849523435199647314d5430e
Vince Gruse <grusvin10@gmail.com>
2024-04-16 20:16:04 -0400

    added signupform component

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import {SignupFormComponent} from "./sign-up-form/sign-up-form.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: "home", component: HomeComponent},
   {path: "recipes", component: RecipesComponent},
-  {path: "recipe/:id", component: RecipeDetailComponent}
+  {path: "recipe/:id", component: RecipeDetailComponent},
+  {path: "sign-up-form", component: SignupFormComponent}
 ];
 
 @NgModule({
