d4fe0ae361c6df400614b3ce932e594a53f2f3da
Vince Gruse <grusvin10@gmail.com>
2024-04-16 20:17:53 -0400

    initial commit

--- back-end/utils/database.py
+++ back-end/utils/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = 'mysql+pymysql://root:root1234@localhost:3306/intelicheff'
+DB_URL = 'mysql+pymysql://root:password@localhost:3306/intellichef'
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
