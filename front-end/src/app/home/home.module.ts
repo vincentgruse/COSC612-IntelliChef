fe13ba0ed40486203f60a6b85ac7f653dbb617c8
Vince Gruse <grusvin10@gmail.com>
2024-04-16 22:10:13 -0400

    started work on sign-in logic on home page

--- front-end/src/app/home/home.module.ts
+++ front-end/src/app/home/home.module.ts
@@ -3,22 +3,27 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { HomeComponent } from './home.component';
 import {RouterModule} from "@angular/router";
 import {MenuComponent} from "../menu/menu.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SignUpComponent} from "../sign-up/sign-up.component";
+import {SignInComponent} from "../sign-in/sign-in.component";
 
 @NgModule({
   declarations: [
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   exports: [
     HomeComponent,
-    MenuComponent
+    MenuComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     NgOptimizedImage,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
