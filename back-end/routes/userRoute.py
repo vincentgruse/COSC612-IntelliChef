4a37eb621fb16448704bf1c21e4ca0451036bbbd
rahal <rahalncm@gmail.com>
2024-04-21 23:55:17 -0400

    login path added

--- back-end/routes/userRoute.py
+++ back-end/routes/userRoute.py
@@ -2,7 +2,7 @@ import base64
 from datetime import datetime
 
 from fastapi import APIRouter, HTTPException, Depends, File, UploadFile, Form
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from starlette import status
 
 import models.models
@@ -10,8 +10,9 @@ from classes.classes import BaseRecipe, BaseIngredient, BaseUser
 from sqlalchemy.orm import Session
 from typing import Annotated
 
+from utils.auth import get_current_user
 from utils.database import SessionLocal
-from utils.util import signJWT
+from utils.util import signJWT, get_hashed_password, verify_password, create_access_token, create_refresh_token
 
 router = APIRouter()
 
@@ -27,26 +28,30 @@ def get_db():
 # annotation for dependency injection
 db_dependency = Annotated[Session, Depends(get_db)]
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-
-@router.get("/login/")
-async def login_user(username: str, password: str, db: db_dependency):
-    user = db.query(models.models.User).filter(models.models.User.username == username, models.models.User.password == password).first()
+@router.post("/login/", summary="Create access and refresh tokens for user")
+async def login_user(db: db_dependency, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = db.query(models.models.User).filter(models.models.User.username == form_data.username).first()
 
     if user is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid login credentials")
-    else:
-        return signJWT(user.id)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username not found!")
 
-@router.get("/user/")
-async def get_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+    hashed_pass = user.password
+    # if not verify_password(form_data.password, hashed_pass):
+    #     raise HTTPException(
+    #         status_code=status.HTTP_400_BAD_REQUEST,
+    #         detail="Incorrect email or password "
+    #     )
+
+    return {
+        "access_token": create_access_token(user.username),
+        "refresh_token": create_refresh_token(user.username),
+    }
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
+# async def get_current_user(token: str):
+#     user = fake_decode_token(token)
+#     return user
 
 
 @router.post("/user")
@@ -57,10 +62,16 @@ async def create_user(new_user: BaseUser, db: db_dependency):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="username already exists")
     # creating a new user
     db_user = models.models.User(**new_user.dict())
+    db_user.password = db_user.password
     # db_user.created_at = datetime.now()
     db.add(db_user)
     db.commit()
     return db_user
+
+
+@router.get('/me', summary='Get details of currently logged in user')
+async def get_me(user: models.models.User = Depends(get_current_user)):
+    return user
 
 
 @router.get("/users/me")
