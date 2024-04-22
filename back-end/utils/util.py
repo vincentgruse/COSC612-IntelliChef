9dd0ed57fc954f06831ce936f4611293415c5443
rahal <rahalncm@gmail.com>
2024-04-21 23:57:53 -0400

    Changes for creating JWT tokens

--- back-end/utils/util.py
+++ back-end/utils/util.py
@@ -1,9 +1,17 @@
 import time
 import jwt
 from decouple import config
+from passlib.context import CryptContext
+import os
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
 
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 JWT_SECRET = config('JWT_SECRET')
 JWT_ALGORITHM = config('JWT_ALGORITHM')
+JWT_REFRESH_SECRET_KEY = config('JWT_REFRESH_SECRET_KEY')  # should be kept secret
 
 
 def signJWT(user_id: str):
@@ -21,3 +29,37 @@ def decodeJWT(token: str):
         return decoded if decoded['exp'] >= time.time() else None
     except:
         return {}
+
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_hashed_password(password: str) -> str:
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    return password_context.verify(password, hashed_pass)
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, JWT_ALGORITHM)
+    return encoded_jwt
+
+
+# Have a longer expiration delay than new access token
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, JWT_ALGORITHM)
+    return encoded_jwt
