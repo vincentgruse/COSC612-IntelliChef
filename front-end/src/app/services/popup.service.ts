f94cf3f5ebad0609ac82e39a842a1385ec0b58cf
Vince Gruse <grusvin10@gmail.com>
2024-04-23 18:14:33 -0400

    Added functionality for submitting new user information

--- front-end/src/app/services/popup.service.ts
+++ front-end/src/app/services/popup.service.ts
@@ -1,18 +1,47 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from "@angular/material/dialog";
 import { SignInComponent } from "../sign-in/sign-in.component";
+import { HttpClient } from '@angular/common/http';
+import { FormGroup } from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PopupService {
-  constructor(private dialog: MatDialog) { }
+  private apiURL = 'http://localhost:8000';
+  constructor(
+    private dialog: MatDialog,
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   openSignInPopup() {
     this.dialog.open(SignInComponent);
   }
 
-  closePopup() {
-    this.dialog.closeAll();
+  submitFormData(form: FormGroup, user: any) {
+    if (form.valid) {
+      // Update user object
+      user.username = form.value.username;
+      user.email = form.value.email;
+      user.password = form.value.password;
+
+      // Send data to backend
+      this.http.post<any>(this.apiURL + "/users", user)
+        .subscribe(
+          response => {
+            // Handle successful response from backend
+            console.log('Backend response:', response);
+          },
+          error => {
+            // Handle error response from backend
+            console.error('Error:', error);
+          }
+        );
+      // Handle form submission
+      console.log(user);
+      this.router.navigate(['/sign-in']);
+    }
   }
 }
