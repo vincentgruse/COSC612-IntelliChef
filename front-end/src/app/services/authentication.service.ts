d35ee49fbd2cc8d4f1ed0e9594d2876320e61bc7
Vince Gruse <grusvin10@gmail.com>
2024-04-23 20:20:24 -0400

    begin work on sign in logic

--- front-end/src/app/services/authentication.service.ts
+++ front-end/src/app/services/authentication.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService { }
+export class AuthenticationService {
+  private apiURL = 'http://localhost:8000';
+  private userStatus = false;
+
+  constructor(private http: HttpClient) { }
+
+  // Method to check if the user is logged in
+  isLoggedIn(): boolean {
+    return this.userStatus;
+  }
+
+  // Method to log out the user
+  logout(): void {
+    this.userStatus = false;
+  }
+}
--- front-end/src/app/sign-in/sign-in.component.ts
+++ front-end/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class SignInComponent {
 
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 }
