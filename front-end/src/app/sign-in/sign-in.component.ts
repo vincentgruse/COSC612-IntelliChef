bb3e7d6b8f0c79174a4d3a85def4d55aea693937
Vince Gruse <grusvin10@gmail.com>
2024-04-26 15:57:58 -0400

    Built sign in form

--- front-end/src/app/sign-in/sign-in.component.ts
+++ front-end/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router'; // Import Router for navigation
 
 @Component({
   selector: 'app-sign-in',
@@ -7,8 +9,35 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  signInForm: FormGroup;
+  errorMessage: string = '';
 
   constructor(
-    private formBuilder: FormBuilder
-  ) { }
+    private authService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router // Inject Router service
+  ) {
+    this.signInForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  submitForm() {
+    if (this.signInForm.valid) {
+      const username = this.signInForm.value.username;
+      const password = this.signInForm.value.password;
+
+      this.authService.login(username, password) // This returns an Observable
+        .subscribe(response => {
+          // Handle successful login (navigate to a different route)
+          this.errorMessage = '';
+          this.router.navigate(['/home']);
+        }, error => {
+          this.errorMessage = 'Invalid username or password';
+          console.error('Login Error:', error);
+        });
+    } else {
+      // Handle form validation errors
+    }
+  }
 }
